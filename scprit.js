4db766ca9c2675f4feec1554e0972934b98589ec
wasim <wasimshariff.02@gmail.com>
2023-06-26 13:47:09 +0530

    added media queries2

--- scprit.js
+++ scprit.js
@@ -1,15 +1,18 @@
 let openBtn = document.querySelector('i[name="open"]')
 let closeBtn = document.querySelector('i[name="close"]')
 let nav = document.querySelector('header ul')
+let header = document.querySelector('header')
 
 openBtn.addEventListener('click',()=>{
     
+    header.classList.toggle('headersactive')
     nav.classList.toggle("active")
     openBtn.classList.toggle("no-display")
     closeBtn.classList.toggle("no-display")
 })
 closeBtn.addEventListener('click',()=>{
     
+    header.classList.toggle('headersactive')
     nav.classList.toggle("active")
     openBtn.classList.toggle("no-display")
     closeBtn.classList.toggle("no-display")
